9f868c2125fb9dbbe9650bb6d41b272c9eda161d
Simmarsandhu <77791732+Simmarsandhu@users.noreply.github.com>
2022-04-14 12:10:46 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,9 +3,56 @@ function start(){
     classifyer=ml5.soundClassifier("https://teachablemachine.withgoogle.com/models/h-k-LCGK1/model.json",modelloaded)
 }
 function modelloaded(){
-    classiyer.classify(gotresult)
+    classifyer.classify(gotresult)
 }
 
-function gotresult(error,results){
+dog = 0;
+cat=0;
+tiger=0;
+hoarse=0;
+
+
+
+function gotresult(error,result){
  console.log("got result")
-}
+
+ if(error){
+     console.log(error);
+
+ }
+ else{
+     console.log(result);
+
+     red= Math.floor(Math.random()*255) +1;
+     green= Math.floor(Math.random()*255)+1;
+     blue = Math.floor(Math.random()*255)+1;
+  
+    document.getElementById("voice").innerHTML= "ANIMAL =" + result[0].label ;
+    document.getElementById("times").innerHTML = "times =" + (result[0].confidence * 100).toFixed(2) + "%"; 
+
+    document.getElementById("voice").style.color="rgb("+red+","+green+","+blue+")";
+    document.getElementById("times").style.color="rgb("+red+","+green+","+blue+")";
+
+    if(result[0].label == "cat" ){
+        document.getElementById("img1").src= "CAT.jpg"
+    }
+    else if(result[0].label == "dog" ){
+        document.getElementById("img1").src= "DOG.jpg"
+    }
+    else if(result[0].label == "tiger" ){
+        document.getElementById("img1").src= "tiger.jpg"
+    }
+    else if(result[0].label == "hoarse" ){
+        document.getElementById("img1").src= "horse.jpg"
+    }
+    else if(result[0].label == "background noise" ){
+        document.getElementById("img1").src= "img2"
+    }
+    
+
+    
+
+
+
+ }
+ }
